3bba9a50bfa73c3bcbf9c90b221fb4a26adf368e
TanakaTakeshikun <131378253+maru0401@users.noreply.github.com>
2023-04-30 11:54:38 +0900

    fix:リンク切れの修正

--- src/component/home/contact.jsx
+++ src/component/home/contact.jsx
@@ -12,7 +12,7 @@ function Contact() {
         <Paper key={notification.id} sx={{ padding: 3, marginBottom: 4 }}>
       <Typography variant="body1" sx={{ mb: 2, color: "black" }}>
         お問い合わせはGithubまで<br />
-        <Link target="_blank" rel="noopener noreferrer" to={"https://github.com/maru0401/saranou-noukan"} >Github</Link><br/>
+        <Link target="_blank" rel="noopener noreferrer" to={"https://github.com/maru0401/noukan"} >Github</Link><br/>
         バグや誤字脱字などはリポジトリにPRしていただけると幸いです。(PR先はDevelopブランチにしてください)<br />
         また、そのほかの問題や、質問などがありましたらissuesでお知らせください。
       </Typography>
