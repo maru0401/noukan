d6c1a6a3ef94e8d0e02804645ee14acf4c4585b6
TanakaTakeshikun <131378253+maru0401@users.noreply.github.com>
2023-04-30 20:09:43 +0900

    fix:お問い合わせが重複していたのを修正

--- src/component/home/contact.jsx
+++ src/component/home/contact.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { Box, Typography, Paper } from '@mui/material';
-import notifications from '../../helpers/news.json';
 
 function Contact() {
   return (
@@ -8,18 +7,16 @@ function Contact() {
       <Typography variant="h4" sx={{ marginBottom: 4, color: "#ffffff" }}>
         お問い合わせ
       </Typography>
-      {notifications.map((notification) => (
-        <Paper key={notification.id} sx={{ padding: 3, marginBottom: 4 }}>
-      <Typography variant="body1" sx={{ mb: 2, color: "black" }}>
-        お問い合わせはGithubまで<br />
-        <Link target="_blank" rel="noopener noreferrer" to={"https://github.com/maru0401/noukan"} >Github</Link><br/>
-        バグや誤字脱字などはリポジトリにPRしていただけると幸いです。(PR先はDevelopブランチにしてください)<br />
-        また、そのほかの問題や、質問などがありましたらissuesでお知らせください。<br/>
-        また、TwitterのDMでも可能です。
-        <Link target="_blank" rel="noopener noreferrer" to={"https://twitter.com/MaruNougyou"} >Twitter</Link><br/>
-      </Typography>
-        </Paper>
-      ))}
+      <Paper sx={{ padding: 3, marginBottom: 4 }}>
+        <Typography variant="body1" sx={{ mb: 2, color: "black" }}>
+          お問い合わせはGithubまで<br />
+          <Link target="_blank" rel="noopener noreferrer" to={"https://github.com/maru0401/noukan"} >Github</Link><br />
+          バグや誤字脱字などはリポジトリにPRしていただけると幸いです。(PR先はDevelopブランチにしてください)<br />
+          また、そのほかの問題や、質問などがありましたらissuesでお知らせください。<br />
+          また、TwitterのDMでも可能です。<br />
+          <Link target="_blank" rel="noopener noreferrer" to={"https://twitter.com/MaruNougyou"} >Twitter</Link><br />
+        </Typography>
+      </Paper>
     </Box>
   );
 }
